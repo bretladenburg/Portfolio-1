7aa2b71ab4b05f292318cc366ba95e2543e8b6cd
Zachary Diehl <zachary.diehl@gmail.com>
2017-04-12 12:52:39 -0700

    created constructor function and object, still working on trying to get the data to pushpopulate on page

--- app.js
+++ app.js
@@ -2,12 +2,24 @@
 
 var projectsArr = [];
 
-function projects(title, description, url) {
-  this.title = title;
-  this.description = description;
-  this.source = source;
-  this.populateData = function() {
-    for(i = 0; i < projectData.length; i++) {
-    }
-  }
+function Project(projectDataObj) {
+  this.title = projectDataObj.title;
+  this.description = projectDataObj.description;
+  this.category = projectDataObj.cateory;
+  this.source = projectDataObj.source;
+  this.img = projectDataObj.img;
 }
+
+Project.prototype.html = function() {
+  var $newProject = $('section.play').clone();
+  $newProject.removeClass('play');
+  $newProject.find('h3').html(this.title);
+  $newProject.find('p').html(this.description);
+  $newProject.find('a').attr('href', this.source);
+  $newProject.find('img').attr('src', this.img);
+  return $newProject.html();
+};
+
+projectsData.forEach(function(projectObj) {
+  projectsArr.push(new Project (projectObj));
+});
---
+++ data.js
@@ -0,0 +1,9 @@
+var projectsData = [
+  {
+    title: 'Bus Mall',
+    description: 'An image survey that takes user inputs and determines most popular items',
+    category: 'professional',
+    source: 'https://github.com/zdiehlio/Bus-Mall',
+    img: 'http://www.freeiconspng.com/uploads/red-bus-icon-8.png',
+  }
+];
